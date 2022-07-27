32b938e3e4f1c3c184092eba8cfa71e6ad838d80
Kehinde Orolade <kehindeorolade@gmail.com>
2022-07-27 14:44:59 +0100

    module1.3 commit

---
+++ module1_lesson_3_assi.py
@@ -0,0 +1,11 @@
+#to find the exponentiation of a number
+print("to find the exponentiation of a number")
+
+x = int(input("Enter first number:"))
+y = int(input("Enter second number:"))
+
+if y==0:
+    print("The answer is 1")
+else:
+    x=x**y
+    print(x)
